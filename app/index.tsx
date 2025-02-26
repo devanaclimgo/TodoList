5480d6ef099c28574e1b76404304b46f0a0a8135
devanaclimgo <anaclimgo@gmail.com>
2025-02-26 20:03:47 -0300

    main content main page

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+   <View style={styles.container}>
+    
+   </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
