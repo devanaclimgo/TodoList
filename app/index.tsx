2960240bbe5443b53e5da92a4465e33e71897d65
devanaclimgo <anaclimgo@gmail.com>
2025-02-26 20:14:40 -0300

    title views

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
    <View style={styles.container}>
-    
+    <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Today's tasks</Text>
+    </View>
    </View>
   )
 }
