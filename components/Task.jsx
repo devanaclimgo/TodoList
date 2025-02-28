6918d9ae7c1ba5afe58710a59cdc4e04fe0e6a6e
devanaclimgo <anaclimgo@gmail.com>
2025-02-28 19:08:22 -0300

    adding main function task file

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,2 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native'
+
+const task = () => {
+  return (
+    <View>
+      <Text>This is a task</Text>
+    </View>
+  )
+}
