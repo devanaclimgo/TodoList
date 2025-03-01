0d19de72568753e7121c0b470f203892789c80e4
devanaclimgo <anaclimgo@gmail.com>
2025-03-01 17:52:32 -0300

    adding circular view task component

--- components/Task.jsx
+++ components/Task.jsx
@@ -3,11 +3,12 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = () => {
   return (
-    <View style={styles.items}>
+    <View style={styles.item}>
       <View style={styles.itemLeft}>
         <TouchableOpacity style={styles.square}></TouchableOpacity>
-        <Text>{props.text}</Text>
+        <Text style={styles.itemText}>{props.text}</Text>
       </View>
+      <View style={styles.circular}></View>
     </View>
   );
 };
