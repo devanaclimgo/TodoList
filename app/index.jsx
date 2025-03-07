a3e94fd9fc8adf65c7de337353ab0672d651a756
devanaclimgo <anaclimgo@gmail.com>
2025-03-06 22:16:14 -0300

    reframing and organizing code

--- app/index.jsx
+++ app/index.jsx
@@ -1,57 +1,69 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput } from 'react-native';
-import Task from '../components/Task';
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import Task from "../components/Task";
 
 export default function HomeScreen() {
-  const [task, setTask] = useState()
-  const [taskItems, setTaskItems] = useState([])
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
   const handleAddTask = () => {
-    setTaskItems([...taskItems, task])
-    setTask(null)
-  }
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
 
   const completeTask = (index) => {
-    let itemsCopy = {...taskItems}
-    itemsCopy.splice(index, 1)
-    setTaskItems(itemsCopy)
-  }
+    let itemsCopy = { ...taskItems };
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
 
   return (
-   <View style={styles.container}>
-    <View style={styles.tasksWrapper}>
-      <Text style={styles.sectionTitle}>Today's tasks</Text>
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
 
-      <View style={styles.items}>
-        {
-          taskItems.map((item, index) => {
+        <View style={styles.items}>
+          {taskItems.map((item, index) => {
             return (
-           <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-              <Task key={index} text={item} />
-            </TouchableOpacity>
-            )
-          })
-        }
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task key={index} text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </View>
-    </View>
 
-    <KeyboardAvoidingView 
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(task)} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(task)}
+        />
 
         <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}></View>
         </TouchableOpacity>
-    </KeyboardAvoidingView>
-   </View>
-  )
+      </KeyboardAvoidingView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e8eaed',
+    backgroundColor: "#e8eaed",
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -59,36 +71,36 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  items:{
+  items: {
     marginTop: 30,
   },
   writeTaskWrapper: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   input: {
     paddingVertical: 15,
     paddingHorizontal: 15,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 60,
-    borderColor: '#C0C0C0',
+    borderColor: "#C0C0C0",
     borderWidth: 1,
     width: 250,
   },
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 60,
-    borderColor: '#C0C0C0',
-    justifyContent: 'center',
-    alignItems: 'center',
+    borderColor: "#C0C0C0",
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 1,
-  }
+  },
 });
